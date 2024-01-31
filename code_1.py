f994bd6d75dc97b7df782b7d6177a1d88e910ed8
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2024-01-31 22:27:29 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+function generateRandomData() {
+
+    data.forEach(item => {
+
+}
+
+    });
+    const data = [];
+        console.log("Random Number: " + item);
+main();
+}
+    const data = generateRandomData();
+    }
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+    return data;function generateRandomData() {
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+    return data;
+
+    }
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    });
+function main() {
+}
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+
+}
